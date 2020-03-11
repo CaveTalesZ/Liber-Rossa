a9b980bb2fe2ec93f78ac3734924442eff0b9fb9
CaveTalesZ <natascha@duijff.nl>
2020-03-11 12:38:16 +0100

    Added grid with control functionality

---
+++ Assets/Scripts/MapControl.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControl : MonoBehaviour
+{
+    public GameObject Selector;
+    private List<GameObject> Rows;
+    private int selectedZone = 0;
+    public float selectionTime = 6.0f;
+    private float timer = 0.0f;
+    public Vector2 selectedSpace = new Vector2(-1, -1);
+
+    //selectedRow and selectedColumn are absolute to the map
+    private int selectedRow = -1;
+    private int selectedColumn = -1;
+
+    //currentRow and currentColumn are relative to the effective area for the selector
+    private int currentRow = 0;
+    private int currentColumn = 0;
+    private int gridSize;
+    private int squareSize;
+    public int baseOffset = 90;
+    public int baseOffsetX = 70;
+    public int baseOffsetZ = 70;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set selector size based on whether one of the four zones has been selected
+        if (selectedZone == 0)
+        {
+            Selector.transform.localScale = new Vector3(5, 1, 5);
+        }
+        else
+        {
+            Selector.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        timer = selectionTime;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Only interact with the selector when the timer goes off
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+
+            timer = selectionTime;
+            // Make a counter increase the selector's column 
+            if (selectedColumn < 0)
+            {
+                currentColumn += 1;
+            }
+            else if (selectedRow < 0)
+            {
+                currentRow += 1;
+            }
+
+            
+            if (selectedZone == 0)
+            {
+                // Change effective area to move grid selector for large area
+                gridSize = 2;
+                squareSize = 50;
+
+                // Cycle through columns, then rows
+                if (currentRow > gridSize - 1)
+                {
+                    currentColumn += 1;
+                }
+                if (currentColumn > gridSize - 1)
+                {
+                    currentRow += 1;
+                }
+            }
+            else
+            {
+                // Change effective area to move grid selector for smaller area
+                gridSize = 5;
+                squareSize = 10;
+            }
+
+
+            currentColumn %= gridSize;
+            currentRow %= gridSize;
+
+            //Move the selector based on current row and column position
+            Selector.transform.localPosition = new Vector3(squareSize * -currentRow + baseOffsetX,
+                                                           Selector.transform.localPosition.y,
+                                                           squareSize * -currentColumn + baseOffsetZ);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (selectedZone == 0)
+            {
+                selectedZone = currentColumn + currentRow * 2 + 1;
+                baseOffsetX = baseOffset - currentRow * squareSize;
+                baseOffsetZ = baseOffset - currentColumn * squareSize;
+                currentRow = 0;
+                currentColumn = -1;
+                Selector.transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (selectedColumn < 0)
+            {
+                selectedColumn = currentColumn + (selectedZone - 1) % 2 * 5;
+                currentRow -= 1;
+            }
+            else if (selectedRow < 0)
+            {
+                selectedRow = currentRow + (selectedZone - 1) / 2 * 5;
+                selectedSpace = new Vector2(selectedColumn, selectedRow);
+                openBuildMenu();
+            }
+            timer = 0.0f;
+        }
+
+    }
+    void openBuildMenu()
+    {
+        Debug.Log("Buildin\' a buildin\'");
+    }
+}
---
+++ Assets/Shaders/Square Outline Shader.shader
@@ -0,0 +1,88 @@
+﻿Shader "Custom/Square Outline Shader"
+{
+    Properties
+    {
+		_MainTexture ("Texture", 2D) = "" {}
+		_Color("Color", Color) = (1,0,0,1)
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+		_Thickness("Thickness", Range(0,10)) = 2
+	}
+
+	SubShader
+	{
+		Tags {"Queue" = "Transparent" "RenderType" = "Transparent" }
+		LOD 100
+
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+
+			CGPROGRAM
+
+			#pragma vertex vertexFunc
+			#pragma fragment fragmentFunc
+
+			#include "UnityCG.cginc"
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f {
+				float4 position: SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			fixed4 _Color;
+			sampler2D _MainTexture;
+			float _Thickness;
+
+			v2f vertexFunc(appdata IN)
+			{
+				v2f OUT;
+				// Slightly enlarge our quad, so we have a margin around it to draw the outline.
+				float expand = 1.0f;
+				IN.vertex.xyz *= expand;
+				OUT.position = UnityObjectToClipPos(IN.vertex);
+				// If we want to get fancy, we could compute the expansion 
+				// dynamically based on line thickness & view angle, but I'm lazy)
+
+				// Expand the texture coordinate space by the same margin, symmetrically.
+				OUT.uv = (IN.uv - 0.5f) * expand + 0.5f;
+				return OUT;
+			}
+
+			fixed4 fragmentFunc(v2f IN) : SV_Target
+			{
+				// Texcoord distance from the center of the quad.
+				float2 fromCenter = abs(IN.uv - 0.5f);
+				// Signed distance from the horizontal & vertical edges.
+				float2 fromEdge = fromCenter - 0.5f;
+
+				// Use screenspace derivatives to convert to pixel distances.
+				fromEdge.x /= length(float2(ddx(IN.uv.x), ddy(IN.uv.x)));
+				fromEdge.y /= length(float2(ddx(IN.uv.y), ddy(IN.uv.y)));
+
+				// Compute a nicely rounded distance from the edge.
+				float distance = abs(min(max(fromEdge.x, fromEdge.y), 0.0f) + length(max(fromEdge, 0.0f)));
+
+				// Sample our texture for the interior.
+				fixed4 pixelColor = float4(0,0,0,0);
+				// Clip out the part of the texture outside our original 0...1 UV space.
+				pixelColor.a *= step(max(fromCenter.x, fromCenter.y), 0.5f);
+
+
+				// Blend in our outline within a controllable thickness of the edge.
+				pixelColor = lerp(pixelColor, _Color, saturate(_Thickness - distance));
+
+				return pixelColor;
+			}
+
+			ENDCG
+		}
+	}
+}
