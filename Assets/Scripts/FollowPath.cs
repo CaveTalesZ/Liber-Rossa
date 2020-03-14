e54843f409c12d79b8ebafcc2b5b94455d592598
CaveTalesZ <natascha@duijff.nl>
2020-03-15 00:55:11 +0100

    Improved path adjustability

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowthePath : MonoBehaviour
+public class FollowPath : MonoBehaviour
 {
+    //Container of all walkpoints
+    public Transform walkpointContainer;
 
     //Array of walkpoints
-    [SerializeField]
-    private Transform[] walkpoints;
+    private List<Transform> walkpoints = new List<Transform>();
 
-    //walk speed
-    private float moveSpeed = 5f;
+    //Walk speed
+    public float moveSpeed = 5.0f;
 
     //Index of current waypoint from which Enemy walks
     //to the next one
     private int waypointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = walkpoints[waypointIndex].transform.position;
+        for (int i = 0; i < walkpointContainer.childCount; i++)
+        {
+            walkpoints.Add(walkpointContainer.GetChild(i));
+        }
+
+        transform.position = walkpoints[waypointIndex].position;
     }
 
     // Update is called once per frame
@@ -31,16 +38,16 @@ public class FollowthePath : MonoBehaviour
     {
         //If enemy didn't reach last waypoint it can move
         //If enemy reached the last, it stops
-        if(waypointIndex <= walkpoints.Length - 1)
+        if(waypointIndex <= walkpoints.Count - 1)
         {
             //Move enemy from current waypoint to the next one
             //using MoveTowards method
-            transform.position = Vector3.MoveTowards(transform.position, walkpoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, walkpoints[waypointIndex].position, moveSpeed * Time.deltaTime);
             
             //if enemy reachers position of waypoint it walked towards
             //then waypointIndex is increased by 1
             //and enemy starts walking to the next waypoint
-            if(transform.position == walkpoints[waypointIndex].transform.position)
+            if(transform.position == walkpoints[waypointIndex].position)
             {
                 waypointIndex += 1;
             }
